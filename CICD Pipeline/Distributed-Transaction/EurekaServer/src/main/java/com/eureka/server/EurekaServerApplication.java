c3354a2a4fca887ca3d822a3b918996b00f9f3f6
Rohit Lodhi <rohitlodhi107@gmail.com>
2024-06-01 14:41:45 +0530

    docke file update

--- CICD Pipeline/Distributed-Transaction/EurekaServer/src/main/java/com/eureka/server/EurekaServerApplication.java
+++ CICD Pipeline/Distributed-Transaction/EurekaServer/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -1,5 +1,7 @@
 package com.eureka.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+ public final static Logger logger  = LoggerFactory.getLogger(EurekaServerApplication.class);
+	
 	public static void main(String[] args) {
+		logger.error("a gya nya build");
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("a gya nya build");
 	}
 
 }
